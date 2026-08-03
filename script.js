2da0b8737c8a356a534496dc43fd5cf077cd13e1
jidevipper <115762688+jidevipper@users.noreply.github.com>
2026-08-03 22:58:58 +0100

    script.js

--- script.js
+++ script.js
@@ -224,37 +224,3 @@ function updateCart() {
     cartCount.textContent = cart.length;
 
 }
-
-cartItems.innerHTML="";
-
-let total=0;
-
-cart.forEach(item=>{
-
-total += item.price;
-
-cartItems.innerHTML += `
-
-<div class="cart-item">
-
-<h4>${item.name}</h4>
-
-<p>$${item.price}</p>
-
-</div>
-
-`;
-
-});
-
-if(cart.length===0){
-
-cartItems.innerHTML="<p>Your cart is empty.</p>";
-
-}
-
-cartTotal.textContent="$"+total;
-
-cartCount.textContent=cart.length;
-
-}
